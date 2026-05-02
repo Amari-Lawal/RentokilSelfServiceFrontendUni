3efb462fd7e820cf9648639c0cccc88bc69a829a
Amari <amari.lawal@gmail.com>
2026-05-02 23:16:31 +0100

    trying to deploy now

--- src/components/AuthForms.jsx
+++ src/components/AuthForms.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-const API_URL = 'http://localhost:8080';
+const API_URL = import.meta.env.VITE_API_URL;
 
 export default function AuthForms({ setToken, setUser }) {
   const [isLogin, setIsLogin] = useState(true);
